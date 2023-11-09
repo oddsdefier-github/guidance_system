e59e4604bbd9418f35c53c4a7e0687c22b3a72d5
Odds <panerjeffryjames@gmail.com>
2023-11-09 12:33:49 +0800

    commit

--- guidance_db.sql
+++ guidance_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2023 at 05:25 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Nov 09, 2023 at 05:33 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,7 +36,7 @@ CREATE TABLE `students` (
   `gender` varchar(40) NOT NULL,
   `Y_level` varchar(50) NOT NULL,
   `contact_no` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
@@ -54,27 +54,30 @@ INSERT INTO `students` (`id`, `student_id`, `first_name`, `middle_name`, `last_n
 
 CREATE TABLE `student_report` (
   `report_no` int(11) NOT NULL,
+  `report_id` varchar(100) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
   `n_student` varchar(50) NOT NULL,
   `teacher` varchar(50) NOT NULL,
   `reasons` varchar(500) NOT NULL,
   `involved_students` varchar(500) NOT NULL,
   `contact_no` text NOT NULL,
-  `date_time` varchar(50) DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
   `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student_report`
 --
 
-INSERT INTO `student_report` (`report_no`, `n_student`, `teacher`, `reasons`, `involved_students`, `contact_no`, `date_time`, `status`) VALUES
-(94, '', 'Roderick Mayo', 'test12', 'test12', '0', '2147483647', 'Approved'),
-(96, '', 'khate ortiz', 'joking', 'test', '0', '2147483647', 'Approved'),
-(103, '', 'Roderick Mayo', 'test', 'test', '0', '2147483647', 'Approved'),
-(104, '', 'khate ortiz', 'joke', 'john', '0', '2147483647', 'Approved'),
-(106, 'john', 'Roderick Mayo', 'test', 'test', '09123456789', '10', 'Approved'),
-(107, 'jay', 'jay', 'test', 'test', '09123456789', '8', 'pending'),
-(108, 'jay', 'khate ortiz', 'test', 'etst', '09123456789', '2-3PM', 'Approved');
+INSERT INTO `student_report` (`report_no`, `report_id`, `user_id`, `n_student`, `teacher`, `reasons`, `involved_students`, `contact_no`, `date_time`, `status`) VALUES
+(111, '', '', 'ROgene', 'Mayo', 'Bullying', 'jaylord', '092929222', '0000-00-00 00:00:00', 'Approved'),
+(112, '16994989753466', '', 'ROgene', 'Mayo', 'Bullying', 'jaylord', '092929222', '0000-00-00 00:00:00', 'Approved'),
+(113, '16994990816723', '', 'ROgene', 'Mayo', 'Bullying', 'jaylord', '092929222', '0000-00-00 00:00:00', 'Approved'),
+(114, '16995009253024', '', 'Jaylord', 'Sir Mayo', 'Nangungulangot sa klase', 'Rogene', '092929222', '0000-00-00 00:00:00', 'Approved'),
+(115, '16995021733248', '3444', 'dfdfdf', 'dfdf', 'ddf', 'fdfd', 'dfdf', '2017-06-01 08:30:00', 'pending'),
+(116, '16995021936136', '3444', 'ddd', 'ddfd', 'dddfdf', 'dfdf', 'ddfdfd', '2017-06-01 08:30:00', 'pending'),
+(117, '16995037327312', '3444', 'Jaylord', 'Sir Mayo', 'Bullying', 'Juan Dela Cruz', '3434343', '2017-06-07 08:30:00', 'pending'),
+(118, '16995038027277', '301830204926171275', 'John', 'Maam Khate', 'Harassament', 'Juan', '243343434343', '2017-06-05 08:30:00', 'pending');
 
 -- --------------------------------------------------------
 
@@ -94,14 +97,14 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL,
   `C_password` varchar(100) NOT NULL,
   `user_type` varchar(50) NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `Student_id`, `first_name`, `middle_name`, `last_name`, `gender`, `Y_level`, `user_name`, `password`, `C_password`, `user_type`) VALUES
-(3444, 'IS001G20', 'Jay Lord', 'Miguel', 'Gan', 'male', 'Grade 11', 'User', '1234', '1234', 'user'),
+(3444, 'IS001G20', 'Jay Lords', 'Miguel', 'Gan', 'male', '', 'User', '12345', '12345', 'user'),
 (5101145, 'Guidance Counsilor', 'Jasper', 'Balani', 'Dequila', 'male', 'Grade 11', 'Admin', '1234', '1234', 'admin'),
 (301830204926171272, '12345est', 'test', 'test', 'test', 'male', 'Grade 12', 'Test123', '1234', '1234', 'user'),
 (301830204926171275, 'IS00131', 'john', 'miguel', 'gan', 'male', '', 'John', '1234', '1234', 'user');
@@ -144,7 +147,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `student_report`
 --
 ALTER TABLE `student_report`
-  MODIFY `report_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `report_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
 -- AUTO_INCREMENT for table `users`
